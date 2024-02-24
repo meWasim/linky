9e8ad3529d8c636d4d803b2f74f8c74c33a38074
meWasim <alfwasim@gmail.com>
2024-02-25 03:30:30 +0530

    dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,10 +79,16 @@
                     <div data-aos="zoom-out">
                         <h1>Make Your Profile With <span>Linky</span></h1>
                         <h2>In one, simple link in bio.</h2>
+                        @auth
+                        <div class="text-center text-lg-start">
+                        <a class="btn-get-started scrollto" href="{{ route('dashboard') }}">LogIn</a>
+                        </div>
+                        @else
                         <div class="text-center text-lg-start">
                             <input type="text" class="scrollto claim-username" name="username" placeholder="Find your username ..." required>
                             <a href="#about" class="btn-get-started scrollto">Continue</a>
                         </div>
+                        @endauth
                     </div>
                 </div>
                 <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
